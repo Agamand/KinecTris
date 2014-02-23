b920d2245ec190356e424a6f4644512d811f92f0
Agamand <basset.cyril@gmail.com>
2014-02-23 19:13:39 +0100

    Implement end party and remove full line

--- grid.py
+++ grid.py
@@ -13,20 +13,22 @@ class Grid():
         self.colors = getColors()
         self.cell = []
         self.b = 0
+        self.isRunning = True
         self.init_grid()
+
 
     def init_grid(self):
         self.setColor(0)
-
     def setColor(self,i):
         colors = self.colors
         scene = bge.logic.getCurrentScene()
-        for x in range(0, CONST_WIDTH):
+        for y in range(0, CONST_HEIGHT):
             self.cell.append([])
-            for y in range(0, CONST_HEIGHT):
+            for x in range(0, CONST_WIDTH):
                 setColor(scene.objects['b_'+str(y)+'_'+str(x)],colors[i])
-                self.cell[x].append(0)
+                self.cell[y].append(0)
     
+
     def setColorAt(self,pos,i):
         if pos[0] >= CONST_WIDTH or pos[0] < 0:
             return;
@@ -53,35 +55,41 @@ class Grid():
             for y in range(0, me.height):
                 if me.cell[x][y] > 0 :
                     self.setColorAt([pos[0]+x,pos[1]+y],me.color)
+
     def getColor(self,pos):
     
         if pos[0] >= CONST_WIDTH or pos[0] < 0:
-            return 1;
+            return 1
         if pos[1] >= CONST_HEIGHT or pos[1] < 0:
-            return 1;
-        return self.cell[pos[0]][pos[1]]
+            return 1
+        return self.cell[pos[1]][pos[0]]
+
     def addToCell(self):
         obj = self.activeObject
         for x in range(0, obj.width):
             for y in range(0, obj.height):
                 if obj.cell[x][y] > 0 :
-                    self.cell[x+obj.pos[0]][y+obj.pos[1]] = obj.color
+                    self.cell[y+obj.pos[1]][x+obj.pos[0]] = obj.color
         self.activeObject = None
         print('cell'+str(self.cell))
+
     def moveActiveObject(self):
         if self.move([0,1]) == 0:
             self.addToCell()
     def move(self,to):
-    
+
+        if self.isRunning == False:
+            return
+
         if self.activeObject is None:
             return 2
         scene = bge.logic.getCurrentScene()
         me = self.activeObject
         pos = me.pos
 
-        if pos[1]+me.height >= CONST_HEIGHT:
-            print("ok fuck1")
-            return 0
+        #if pos[1]+me.height >= CONST_HEIGHT:
+        #    print("ok fuck1")
+        #    return 0
         print('obj pos : '+str(pos))
 
         #for x in range(0, me.width):
@@ -107,12 +115,17 @@ class Grid():
         return 1
 
     def update(self):
+        if self.isRunning == False:
+            return
+
         self.moveActiveObject()
         if self.activeObject is None:
             self.spawn()
 
 
     def rotate(self,to):
+        if self.isRunning == False:
+            return
 
         if self.activeObject is None:
             return 2
@@ -131,7 +144,6 @@ class Grid():
                 if me.cell[x][y] > 0 :
                     self.setColorAt([pos[0]+x,pos[1]+y],0)
 
-
         me.setPattern(to)
 
         for x in range(0, me.width):
@@ -141,13 +153,42 @@ class Grid():
 
 
     def spawn(self):
-        self.setActiveObject(Block(getPattern()[self.b]))
+
+        if self.isRunning == False:
+            return
+
+        self.removeFullLine()
+
+        newBlock = Block(getPattern()[self.b])
+
+        if self.testPattern(newBlock.cell,newBlock.width,newBlock.height,newBlock.pos) < 1:
+            print('game end !')
+            self.isRunning = False
+            return
+
+        self.setActiveObject(newBlock)
         self.b = self.b + 1
         if self.b > 6:
             self.b = 0
 
 
+    def removeFullLine(self):
+        for y in range(0, CONST_HEIGHT):
+            full = True
+            for x in range(0, CONST_WIDTH):           
+                if self.cell[y][x] < 1 :
+                    full = False
+            if full:
+                del self.cell[y]
+                self.cell.insert(0,[])
+                for x in range(0, CONST_WIDTH):
+                    self.cell[0].append(0)
 
+        for y in range(0, CONST_HEIGHT):
+            self.cell.append([])
+            for x in range(0, CONST_WIDTH):
+                self.setColorAt([x,y],self.cell[y][x])
+                    
 
     def testPattern(self,pattern,h,w,pos):
         for x in range(0, w):
@@ -155,7 +196,6 @@ class Grid():
                 if pattern[x][y] > 0 and self.getColor([pos[0]+x,pos[1]+y]) > 0:
                     return 0
         return 1
-
 def main():
     Grid.getInstance().spawn()
 
